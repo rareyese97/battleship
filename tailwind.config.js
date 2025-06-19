4a89a43567524387ad48956f4c4a0a9e0957cb9d
reyesenamorado97 <reyesenamorado@googlemail.com>
2025-06-19 11:09:32 -0400

    testing still

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,7 @@
+// frontend/tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./app/**/*.{js,ts,jsx,tsx}",
-		"./components/**/*.{js,ts,jsx,tsx}",
-		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./src/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-		extend: {
-			animation: {
-				"strong-pulse": "strong-pulse 0.6s ease-in-out infinite",
-			},
-			keyframes: {
-				"strong-pulse": {
-					"0%, 100%": {
-						backgroundColor: "rgba(34, 197, 94, 0.8)", // GREEN 500
-						boxShadow: "0 0 10px rgba(34, 197, 94, 0.8), 0 0 20px rgba(34, 197, 94, 0.8)", // GREEN glow
-					},
-					"50%": {
-						backgroundColor: "rgba(21, 128, 61, 0.5)", // GREEN 700 darker
-						boxShadow: "0 0 20px rgba(34, 197, 94, 1), 0 0 30px rgba(34, 197, 94, 1)", // GREEN glow
-					},
-				},
-			},
-		},
-	},
-
+	content: ["./src/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+	theme: { extend: {} },
 	plugins: [],
 };
