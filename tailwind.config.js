e22dfb223a318e345c66f289476eb96bfde2ffba
reyesenamorado97 <reyesenamorado@googlemail.com>
2025-06-19 10:48:39 -0400

    testing animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,11 +14,11 @@ module.exports = {
 			keyframes: {
 				"strong-pulse": {
 					"0%, 100%": {
-						backgroundColor: "rgba(220, 38, 38, 0.8)", 
+						backgroundColor: "rgba(220, 38, 38, 0.6)", 
 						boxShadow: "0 0 10px rgba(250, 204, 21, 0.8), 0 0 20px rgba(250, 204, 21, 0.8)",
 					},
 					"50%": {
-						backgroundColor: "rgba(185, 28, 28, 0.5)", 
+						backgroundColor: "rgba(185, 28, 28, 0.3)", 
 						boxShadow: "0 0 20px rgba(250, 204, 21, 1), 0 0 30px rgba(250, 204, 21, 1)",
 					},
 				},
