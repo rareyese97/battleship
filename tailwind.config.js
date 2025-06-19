388fac59a9591552577537377dfc2d0d326acf53
reyesenamorado97 <reyesenamorado@googlemail.com>
2025-06-19 10:22:03 -0400

    attempting to fix pulse

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 // frontend/tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
-	theme: { extend: {} },
+	content: [
+		"./src/**/*.{js,ts,jsx,tsx}",
+		"./app/**/*.{js,ts,jsx,tsx}",
+		// etc
+	],
+	safelist: ["animate-strong-pulse"],
+	theme: {
+		extend: {},
+	},
 	plugins: [],
 };
