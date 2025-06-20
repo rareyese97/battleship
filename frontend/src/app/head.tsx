e5044ad5149084854c586c7b7e669103dd91d686
reyesenamorado97 <reyesenamorado@googlemail.com>
2025-06-20 11:59:39 -0400

    Added favicons

---
+++ frontend/src/app/head.tsx
@@ -0,0 +1,13 @@
+// app/head.tsx
+export default function Head() {
+	return (
+		<>
+			<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+			<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+			<link rel="shortcut icon" href="/favicon.ico" />
+			<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+			<meta name="apple-mobile-web-app-title" content="Sink That Ship" />
+			<link rel="manifest" href="/site.webmanifest" />
+		</>
+	);
+}
